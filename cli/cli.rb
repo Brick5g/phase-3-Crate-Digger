c080f346600882c754d458e1a9be14265d55d0ef
Julian Francis <jgfrancis3@gmail.com>
2026-07-28 08:55:35 -0400

    Artist Selection

--- cli/cli.rb
+++ cli/cli.rb
@@ -98,10 +98,74 @@ end
     end
   end
 
-  def select_artist
+ def select_artist
+  artists = Artist.order(:name)
+
+  if artists.empty?
     puts
-    puts "Select an artist is coming soon."
+    puts "No artists found."
+    return
   end
+
+  puts
+  puts "Select an Artist"
+  puts "----------------"
+
+  artists.each_with_index do |artist, index|
+    puts "#{index + 1}. #{artist.name}"
+  end
+
+  print "> "
+  choice = gets.chomp.to_i
+
+  if choice < 1 || choice > artists.length
+    puts
+    puts "Invalid artist selection."
+    return
+  end
+
+  selected_artist = artists[choice - 1]
+
+  artist_menu(selected_artist)
+end
+
+def artist_menu(artist)
+  loop do
+    puts
+    puts "Artist: #{artist.name}"
+    puts "Genre: #{artist.genre}"
+    puts "Hometown: #{artist.hometown}"
+    puts
+    puts "1. View releases"
+    puts "2. Add a release"
+    puts "3. Update artist"
+    puts "4. Delete artist"
+    puts "5. Back to main menu"
+    print "> "
+
+    choice = gets.chomp
+
+    case choice
+    when "1"
+      puts
+      puts "View releases for #{artist.name} is coming soon."
+    when "2"
+      puts
+      puts "Add a release for #{artist.name} is coming soon."
+    when "3"
+      puts
+      puts "Update #{artist.name} is coming soon."
+    when "4"
+      puts
+      puts "Delete #{artist.name} is coming soon."
+    when "5"
+      break
+    else
+      puts
+      puts "Invalid selection. Please choose a number from 1 to 5."
+    end
+  end
+end
 
   def view_releases
     puts
